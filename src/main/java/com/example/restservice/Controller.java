70aac55531d6a5a0bde2c18897dc2335ce6dc1aa
Nathan Nguyen <natnguyen@blackberry.com>
2023-06-06 16:07:28 -0700

    WIP

--- src/main/java/com/example/restservice/Controller.java
+++ src/main/java/com/example/restservice/Controller.java
@@ -2,9 +2,12 @@ package com.example.restservice;
 
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
@@ -16,32 +19,69 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Controller {
     ConcurrentHashMap<String, Record> db = new ConcurrentHashMap<>();
 
+		// can move this to somewhere else later
+		public boolean verify_uuid(String id) {
+			try {
+				UUID.fromString(id);
+			} catch(IllegalArgumentException e) {
+				return false;
+			}
+			return true;
+		}
+
     @PostMapping(value="/guid/{id}")
     public ResponseEntity f(@PathVariable("id") String id) {
-	try {
-	    UUID.fromString(id);
-	} catch(IllegalArgumentException e) {
-	    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-	}
-	long expire=0;
-	String user="user";
-	Record rec = new Record(id, expire, user);
-	db.put(id, rec);
-	return ResponseEntity.ok().body(rec);
+			if (!verify_uuid(id)) {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			long expire=0;
+			String user="user";
+			Record rec = new Record(id, expire, user);
+			db.put(id, rec);
+			return ResponseEntity.ok().body(rec);
     }
 
     @GetMapping(value="/guid/{id}")
     public ResponseEntity g(@PathVariable("id") String id) {
-	try {
-	    UUID.fromString(id);
-	} catch(IllegalArgumentException e) {
-	    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-	}
-	Record rec = db.get(id);
-	if (rec == null) {
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-	} else {
-	    return ResponseEntity.ok().body(db.get(id));
-	}
+			if (!verify_uuid(id)) {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			Record rec = db.get(id);
+			if (rec == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+					return ResponseEntity.ok().body(db.get(id));
+			}
     }
+
+		@PutMapping(value = "/guid/{id}")
+		public ResponseEntity h(@PathVariable("id") String id) {
+			if (!verify_uuid(id)) {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			Record rec = db.get(id);
+			if (rec == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				long expire=0;
+				String user="new_user";
+				Record rec_new = new Record(id, expire, user);
+				db.put(id, rec_new);
+				return ResponseEntity.ok().body(db.get(id));
+			}
+		}
+
+		@DeleteMapping(value = "/guid/{id}")
+		public ResponseEntity i(@PathVariable("id") String id) {
+			if (!verify_uuid(id)) {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			Record rec = db.get(id);
+			if (rec == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				db.remove(id);
+				return ResponseEntity.ok().body("");
+			}
+		}
 }
