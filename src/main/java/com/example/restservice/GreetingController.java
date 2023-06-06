5af0bc6778832f79d4c2caa60e0343fe2a517e33
John de la Garza <jdelagarza@cylance.com>
2023-06-06 13:54:52 -0700

    WIP

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,19 +1,32 @@
 package com.example.restservice;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 @RestController
-public class GreetingController {
+public class Controller {
+    ConcurrentHashMap<String, Record> db = new ConcurrentHashMap<>();
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    @PostMapping(value="/guid/{id}")
+    public ResponseEntity greeting(@PathVariable("id") String id) {
+	try {
+	    UUID.fromString(id);
+	} catch(IllegalArgumentException e) {
+	    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
+	long expire=0;
+	String user="user";
+	Record rec = new Record(id, expire, user);
+	db.put(id, rec);
+	return ResponseEntity.ok().body(rec);
+}
 }
---
+++ src/main/java/com/example/restservice/Record.java
@@ -0,0 +1,3 @@
+package com.example.restservice;
+
+public record Record(String id, long ts, String user) { }
